4ee24442d814e7fe4c4b7c8d0d3ccbdd1646cf14
romulo <romulodmilani@gmail.com>
2023-04-18 10:47:59 -0300

    created the basic structure in html and css to be able to test the logic

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//MY ENTIRE LIBRARY IS IN THIS ARRAY
 let myLibrary = [];
+
+//MODEL FOR THE BOOKS
 
 function Book(title, author, pages, read) {
   (this.title = title),
@@ -6,6 +9,8 @@ function Book(title, author, pages, read) {
     (this.pages = pages),
     (this.read = read);
 }
+
+//FUNCTION THAT DISPLAYS THE INFO OF JUST ONE CHOSEN BOOK, ERASE IF NOT NEEDED!!!!
 
 Book.prototype.info = function () {
   if (read == true) {
@@ -15,6 +20,31 @@ Book.prototype.info = function () {
   }
 };
 
-function addBookToLibrary(objName) {
-  myLibrary.push(objName);
+//ADD A NEW BOOK TO THE TABLE
+
+function addBookToLibrary(bookName) {
+  myLibrary.push(bookName);
 }
+
+//SHOWS THE ENTIRE LIBRARY
+function showLibrary() {
+  myLibrary.forEach((element) => {
+    console.log(`Book title: ${element.title}\n`);
+    console.log(`Author: ${element.author}\n`);
+    console.log(`Number of pages: ${element.pages}`);
+    console.log(`Read? ${element.read}\n\n`);
+  });
+}
+
+/* ADDING A FEW TEST BOOKS
+
+const hp = new Book("HP and the Phylosopher Stone", "JK Roling", 200, true);
+const hobbit = new Book("Hobbit", "Tolkien", 300, true);
+const it = new Book("it", "Stephen King", 1200, false);
+
+addBookToLibrary(hp);
+addBookToLibrary(hobbit);
+addBookToLibrary(it);
+
+showLibrary();
+*/
